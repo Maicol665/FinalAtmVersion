c427eab940d62689514177c1243e8cdd95be67f7
Michael665 <maicolbeltran885@gmail.com>
2025-09-27 23:57:34 -0500

    Creacion de mi primera clase : (Cuenta) y definicion de sus  atributos

---
+++ FinalAtmVersion/Clases/Cuenta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalAtmVersion.Clases
+{
+    internal class Cuenta
+    {
+        public int NumeroCuenta { get; set; }
+        public string Pin { get; set; }
+        public decimal Saldo { get; set; }
+        public List<string> Movimientos { get; set; } = new List<string>();
+    }
+}
