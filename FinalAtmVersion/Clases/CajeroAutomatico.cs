ca55b370901d0b2b055bd4be933fae29e0f5c7a6
Michael665 <maicolbeltran885@gmail.com>
2025-09-28 12:29:10 -0500

    Creacion de constantes: archivos planos donde se guardaran las cuentas y los movimientos en la clase CajeroAutomatico

---
+++ FinalAtmVersion/Clases/CajeroAutomatico.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalAtmVersion.Clases
+{
+    internal class CajeroAutomatico
+    {
+        private const string ArchivoCuentas = "cuentas.txt"; 
+        private const string ArchivoMovimientos = "movimientos_{0}.txt";
+        private Cuenta cuentaActual;
+    }
+}
